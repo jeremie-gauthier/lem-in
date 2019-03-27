27268b4bd381f4f98b20e4d4e608519a50c88ae1
Jeremie <jeremiegthr@gmail.com>
2019-03-28 00:44:03 +0100

    fix - in room name

--- srcs/register_room.c
+++ srcs/register_room.c
@@ -7,21 +7,31 @@ static int	clean_quit(char **str, t_room **room)
 	return (FAIL);
 }
 
+/*
+**	Duplicate the name of a room in a fresh memory string.
+**	The name is contained between the first char and the 'limit' char.
+**	It also checks for the validity of the room's name ('-' not included)
+*/
+
 static char	*ft_strcdup(const char *buf, int *i, const char limit)
 {
 	int		len;
 	char	*new;
 	int		tmp;
 
-	len = 0;
-	tmp = 0;
-	if (buf == NULL)
+	if (buf == NULL || buf[0] == 'L')
 		return (NULL);
-	while (buf && buf[len] != limit)
+	len = 0;
+	while (buf[len] && buf[len] != limit)
+	{
+		if (buf[len] == '-')
+			return (NULL);
 		len++;
+	}
 	if (!(new = (char*)malloc(sizeof(*new) * len + 1)))
 		return (NULL);
 	*i += len + 1;
+	tmp = 0;
 	while (tmp < len)
 	{
 		new[tmp] = buf[tmp];
@@ -34,8 +44,10 @@ static char	*ft_strcdup(const char *buf, int *i, const char limit)
 static int	ft_search_duplicate(const t_room *room, t_btree *graph)
 {
 	t_room	*ret;
+	int		(*cmpf)(const void *, const void *);
 	
-	ret = (t_room*)btree_search_data(graph, (void*)room, ft_btreecmp);
+	cmpf = &ft_btreecmp;
+	ret = (t_room*)btree_search_data(graph, (void*)room, cmpf);
 	if (ret == NULL)
 		return (0);
 	return (1);
@@ -52,13 +64,14 @@ static void	ft_register_start_end(t_parser *data, t_room *room)
 	}
 }
 
-int			ft_register_room(const char *buf, int *i, t_parser *data, t_btree **graph)
+int			ft_register_room(const char *buf, int *i, t_parser *data,
+				t_btree **graph)
 {
 	char	*name;
 	t_room	*room;
+	int		(*cmpf)(const void *, const void *);	
 
-	if (buf[*i] == 'L')
-		return (FAIL);
+	cmpf = &ft_btreecmp;
 	if (!(name = ft_strcdup(&buf[*i], i, ' ')))
 		return (FAIL);
 	if (!(room = init_room(name, data->status)))
@@ -67,7 +80,7 @@ int			ft_register_room(const char *buf, int *i, t_parser *data, t_btree **graph)
 		return (clean_quit(NULL, &room));
 	if (buf[*i] == ' ')
 		return (clean_quit(NULL, &room));
-	if (!(btree_insert_data(graph, (void*)room, ft_btreecmp)))
+	if (!(btree_insert_data(graph, (void*)room, cmpf)))
 		return (clean_quit(NULL, &room));
 	if (!(ft_register_x(&buf[*i], i, room)))
 		return (FAIL);
