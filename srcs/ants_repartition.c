7d9823517a5a3657764ade3f47de00604f0360e0
Caroline Moulinier <igor@MacBook-Pro-de-Caroline.local>
2019-04-22 12:24:47 +0200

    bug sort

--- srcs/ants_repartition.c
+++ srcs/ants_repartition.c
@@ -24,14 +24,35 @@ static t_room	**init_roomtab(t_parser *data, int flow_max)
 	return (new);
 }
 
-static void	ft_sort_roomtab(t_room **tab)
+static void	ft_sort_roomtab(t_room ***tab, int len)
 {
+	t_room *tmp;
+	int		i;
+	int		j;
 
+	i = 0;
+	while (i < len - 1)
+	{
+		j = i + 1;
+		while (j < len)
+		{
+			ft_printf("%s\n", (*tab[1])->name);
+			if ((*tab[i])->len_path > (*tab[j])->len_path)
+			{
+				tmp = *tab[i];
+				*tab[i] = *tab[j];
+				*tab[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
 }
 
 static void	ft_balance_ants(t_room **tab, int len)
 {
-
+	(void)tab;
+	(void)len;
 }
 
 int			ft_ants_repartition(t_parser *data, int flow_max)
@@ -40,7 +61,19 @@ int			ft_ants_repartition(t_parser *data, int flow_max)
 
 	if (!(tab = init_roomtab(data, flow_max)))
 		return (FAIL);
-	ft_sort_roomtab(tab);
+	int j = 0;
+	while (j < flow_max)
+	{
+		ft_printf("avant %s -> %i\n", tab[j]->name, tab[j]->len_path);
+		j++;
+	}
+	ft_sort_roomtab(&tab, flow_max);
+	int i = 0;
+	while (i < flow_max)
+	{
+		ft_printf("apres %s -> %i\n", tab[i]->name, tab[i]->len_path);
+		i++;
+	}
 	ft_balance_ants(tab, flow_max);
 	ft_memdel((void*)&tab);
 	return (SUCCESS);
